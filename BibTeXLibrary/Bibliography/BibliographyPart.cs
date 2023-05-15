2a2d52e307604081d7cae00afe79977c6ca58245
Lance Endres <lanceendres@gmail.com>
2023-05-14 19:39:44 -0500

    Remove unused using.

--- BibTeXLibrary/Bibliography/BibliographyPart.cs
+++ BibTeXLibrary/Bibliography/BibliographyPart.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
 
 namespace BibTeXLibrary
 {
