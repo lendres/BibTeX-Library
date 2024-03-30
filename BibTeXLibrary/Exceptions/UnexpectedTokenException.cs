bff0a7dec18772bed39239b9c3b0f30f36e56aaf
lendres <lanceendres@gmail.com>
2024-03-29 20:41:41 -0500

    Update UnexpectedTokenException.cs

--- BibTeXLibrary/Exceptions/UnexpectedTokenException.cs
+++ BibTeXLibrary/Exceptions/UnexpectedTokenException.cs
@@ -17,21 +17,28 @@ namespace BibTeXLibrary
 
         #region Constructor
 
-        public UnexpectedTokenException(int lineNumber, int colNumber, TokenType unexpected, params TokenType[] expected)
-            : base(lineNumber, colNumber)
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="lineNumber">Line number where the token was found.</param>
+		/// <param name="columnNumber">Column number where the token was found.</param>
+		/// <param name="unexpectedToken">The token found.</param>
+		/// <param name="expectedTokens">The list of tokens that were expected/allowed.</param>
+        public UnexpectedTokenException(int lineNumber, int columnNumber, TokenType unexpectedToken, params TokenType[] expectedTokens)
+            : base(lineNumber, columnNumber)
         {
-			StringBuilder errorMsg = new StringBuilder($"An unexpected token was found.\nToken: '{unexpected}'.\nAt line {lineNumber}, column {colNumber}.");
+			StringBuilder errorMsg = new StringBuilder($"An unexpected token was found.\nToken: '{unexpectedToken}'.\nAt line {lineNumber}, column {columnNumber}.");
 
 			// Add a list of acceptable tokens.
             errorMsg.Append("\nExpected: ");
-            foreach (TokenType item in expected)
+            foreach (TokenType item in expectedTokens)
             {
                 errorMsg.Append($"{item}, ");
             }
 			// Remove last comma and space.
             errorMsg.Remove(errorMsg.Length - 2, 2);
 
-            Message = errorMsg.ToString();
+            this.Message = errorMsg.ToString();
         }
 
         #endregion
