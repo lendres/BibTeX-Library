8d50fdcc0e7bd873b8ca8e8dab8df6ac4de23886
Ikosy <lendres@fifthrace.com>
2024-11-21 06:28:59 -0600

    Update AssemblyInfo.cs

--- src/BibTeXLibrary/Properties/AssemblyInfo.cs
+++ src/BibTeXLibrary/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BibTeXLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("e453ff0b-97b0-4575-9a2e-b15dcbf7c47f")]
 
-[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
